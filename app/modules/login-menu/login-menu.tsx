e13c6c3afe8d38a4a0772a7bb82295575db1677b
karimadnan <karimmadnan@gmail.com>
2024-07-11 03:05:55 +0300

    add: login menu fallback loading

---
+++ app/modules/login-menu/login-menu.tsx
@@ -0,0 +1,22 @@
+import LoginForm from "../login-form/login-form";
+import { auth } from "@karimACC/app/auth";
+import LoggedIn from "./logged-in";
+import { Suspense } from "react";
+import Paper from "@karimACC/app/ui/components/paper";
+
+export default async function LoginMenu() {
+  const session = await auth();
+  const user = session?.user;
+
+  return (
+    <Paper className="p-5">
+      {!user?.id ? (
+        <LoginForm />
+      ) : (
+        <Suspense fallback={<h1>Loading</h1>}>
+          <LoggedIn />
+        </Suspense>
+      )}
+    </Paper>
+  );
+}
