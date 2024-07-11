912c7dd1b121e1fba8297f6bd34338ec33a00b76
karimadnan <karimmadnan@gmail.com>
2024-07-11 03:07:42 +0300

    chore: buttons colors shade

--- app/ui/components/button.tsx
+++ app/ui/components/button.tsx
@@ -11,17 +11,14 @@ const buttonVariants = cva(
       variant: {
         default:
           "bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90",
-        destructive:
-          "bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90",
+        destructive: "bg-red-600 text-slate-50 hover:bg-red-600/90",
         outline:
           "border border-slate-200 bg-white hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50",
-        secondary:
-          "bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
+        secondary: "bg-sky-600 text-slate-50 hover:bg-sky-600/80",
         ghost:
           "hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
         link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
-        positive:
-          "text-slate-50 bg-green-600 hover:bg-green-600/90 dark:bg-green-900 dark:text-slate-50 dark:hover:bg-green-900/90",
+        positive: "text-slate-50 bg-green-600 hover:bg-green-600/90",
       },
       size: {
         default: "h-10 px-4 py-2",
