cc4bd95ed229f4b74798bb3208103e7ee2e4cdb2
karimadnan <karimmadnan@gmail.com>
2024-07-11 03:08:50 +0300

    add: vocation utils

---
+++ app/util/vocation.ts
@@ -0,0 +1,12 @@
+import { Vocations } from "../models/player";
+
+export const VOCATION_ID_TO_NAME: Record<number, Vocations> = {
+  1: "Sorcerer",
+  2: "Druid",
+  3: "Paladin",
+  4: "Knight",
+  5: "Master Sorcerer",
+  6: "Elder Druid",
+  7: "Royal Paladin",
+  8: "Elite Knight",
+};
