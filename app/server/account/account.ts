ede73a2c8293099990cbe81fb5a1800386d1fd5f
karimadnan <karimmadnan@gmail.com>
2024-07-11 03:06:55 +0300

    init create account API & bcrypt skeleton

--- app/server/account/account.ts
+++ app/server/account/account.ts
@@ -1,3 +1,8 @@
+"use server";
+
+import bcrypt from "bcrypt";
+import { CreateAccountModel } from "@karimACC/app/models/common";
+import { createAccountSchema } from "@karimACC/app/models/schemas";
 import { prisma } from "@karimACC/app/util/prisma";
 
 export async function getAccountInfo(accountId: number) {
@@ -7,9 +12,36 @@ export async function getAccountInfo(accountId: number) {
       coins_transferable: true,
       email: true,
       premdays: true,
-      players: true,
+      players: {
+        select: {
+          name: true,
+          level: true,
+          id: true,
+          PlayerOnline: true,
+          vocation: true,
+        },
+      },
     },
   });
 
   return accountInfo || null;
+}
+
+export async function createAccount(accountInfo: CreateAccountModel) {
+  const validateData = createAccountSchema.safeParse(accountInfo);
+
+  if (validateData.success === false) {
+    return { message: "Invalid account data." };
+  }
+
+  const isDuplicate = await prisma.accounts.findUnique({
+    where: { email: accountInfo.email },
+  });
+
+  if (isDuplicate) return { message: "Email already exists." };
+
+  const accountDataClone = structuredClone(accountInfo);
+  accountDataClone.sex = Number(accountDataClone.sex);
+
+  const passwordHash = bcrypt.hashSync(accountInfo.password, 10);
 }
