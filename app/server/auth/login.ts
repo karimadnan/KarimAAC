ed443e9bb4e797a52c8f3e172b63532b42437070
karimadnan <karimmadnan@gmail.com>
2024-07-11 03:07:16 +0300

    chore: add catch block to login API

--- app/server/auth/login.ts
+++ app/server/auth/login.ts
@@ -1,15 +1,21 @@
+"use server";
+
 import { prisma } from "@karimACC/app/util/prisma";
 
 export async function dbLogin(
   credentials: Partial<Record<"email" | "password", unknown> | undefined>
 ) {
-  const findAccount = await prisma.accounts.findUnique({
-    where: { email: String(credentials?.email) },
-  });
+  try {
+    const findAccount = await prisma.accounts.findUnique({
+      where: { email: String(credentials?.email) },
+    });
 
-  if (!findAccount || findAccount.password !== credentials?.password) {
-    return false;
+    if (!findAccount || findAccount.password !== credentials?.password) {
+      return false;
+    }
+
+    return findAccount;
+  } catch (_) {
+    throw new Error("DB error");
   }
-
-  return findAccount;
 }
