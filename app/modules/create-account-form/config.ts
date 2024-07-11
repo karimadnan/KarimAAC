c2965f13a2d539cc4ae8a9003d9351dbf39b7c96
karimadnan <karimmadnan@gmail.com>
2024-07-11 03:04:52 +0300

    add: starting vocations options object

---
+++ app/modules/create-account-form/config.ts
@@ -0,0 +1,24 @@
+import { VOCATION_ID_TO_NAME } from "@karimACC/app/util/vocation";
+
+export const VOCATIONS_OPTIONS = [
+  {
+    name: VOCATION_ID_TO_NAME[1],
+    value: 1,
+    image: "/Sorceress.png",
+  },
+  {
+    name: VOCATION_ID_TO_NAME[2],
+    value: 2,
+    image: "/Druid.png",
+  },
+  {
+    name: VOCATION_ID_TO_NAME[3],
+    value: 3,
+    image: "/Paladin.png",
+  },
+  {
+    name: VOCATION_ID_TO_NAME[4],
+    value: 4,
+    image: "/Knight.png",
+  },
+];
