98bc15af4fd53e7825a0fe3761ecb670b8ab2444
karimadnan <karimmadnan@gmail.com>
2024-08-17 19:50:03 +0300

    chore: create account action & helpers

--- app/server/account/account.ts
+++ app/server/account/account.ts
@@ -1,9 +1,22 @@
 "use server";
 
-import bcrypt from "bcrypt";
 import { CreateAccountModel } from "@karimACC/app/models/common";
 import { createAccountSchema } from "@karimACC/app/models/schemas";
 import { prisma } from "@karimACC/app/util/prisma";
+import {
+  getVocationSkeleton,
+  isCharacterNameDuplicate,
+} from "../character/character";
+import { constructAccountData } from "./utils";
+import { nodemailerTransporter } from "@karimACC/app/util/nodemailer";
+
+const isEmailDuplicate = async (email: string) => {
+  const isDuplicateEmail = await prisma.accounts.findUnique({
+    where: { email: email },
+  });
+
+  if (isDuplicateEmail) return { message: "Email already exists." };
+};
 
 export async function getAccountInfo(accountId: number) {
   const accountInfo = await prisma.accounts.findUnique({
@@ -27,21 +40,91 @@ export async function getAccountInfo(accountId: number) {
   return accountInfo || null;
 }
 
-export async function createAccount(accountInfo: CreateAccountModel) {
+export async function createAccountAction(accountInfo: CreateAccountModel) {
+  // Check if data struct is as expected
   const validateData = createAccountSchema.safeParse(accountInfo);
 
   if (validateData.success === false) {
     return { message: "Invalid account data." };
   }
 
-  const isDuplicate = await prisma.accounts.findUnique({
-    where: { email: accountInfo.email },
+  // Check duplicate info
+  const isDuplicateEmail = await isEmailDuplicate(accountInfo.email);
+  if (isDuplicateEmail) return { message: isDuplicateEmail.message };
+
+  const isDuplicateCharacterName = await isCharacterNameDuplicate(
+    accountInfo.characterName
+  );
+
+  if (isDuplicateCharacterName)
+    return { message: isDuplicateCharacterName.message };
+
+  // Get vocation template data to populate character info
+  const vocationSample = await getVocationSkeleton(
+    Number(accountInfo.vocation)
+  );
+  if (!vocationSample)
+    return { message: "Failed to load vocation data please report to GM." };
+
+  vocationSample.name = accountInfo.characterName;
+
+  const verifySecret = crypto.randomUUID();
+
+  const creationData = constructAccountData(
+    accountInfo,
+    vocationSample,
+    verifySecret
+  );
+
+  try {
+    await prisma.accounts.create({
+      data: {
+        ...creationData.account,
+        players: { createMany: { data: [creationData.characterData] } },
+      },
+    });
+  } catch (error) {
+    return { message: "Failed while trying to create account." };
+  }
+
+  await nodemailerTransporter.sendMail({
+    from: "KarimAAC",
+    to: "kimocaramila@gmail.com",
+    subject: "Verify your KarimAAC account",
+    html: `
+    <main>
+      <b>SERVER NAME verify your account</b>
+
+      <p>Clicking on the following link will <a href="${process.env.APP_URL}/verify/${verifySecret}">verify</a> your account.</p>
+      If you did not sign up please ignore this email.
+    </main>
+    `,
+  });
+}
+
+export async function verifyEmail(secret: string) {
+  const findEmail = await prisma.accounts.findFirst({
+    where: {
+      verify_token: secret,
+    },
   });
 
-  if (isDuplicate) return { message: "Email already exists." };
+  if (findEmail?.verified) return { message: "Email is already verified!" };
 
-  const accountDataClone = structuredClone(accountInfo);
-  accountDataClone.sex = Number(accountDataClone.sex);
+  if (!findEmail) return { message: "Invalid link" };
 
-  const passwordHash = bcrypt.hashSync(accountInfo.password, 10);
+  const accountVerify = await prisma.accounts.update({
+    where: {
+      id: findEmail.id,
+    },
+    data: {
+      verified: true,
+      verify_token: null,
+    },
+  });
+
+  if (!accountVerify.verified)
+    return {
+      message: "Failed to update verification status, please try again later!",
+    };
 }
